17e811285f2d19c03d06abd20a70b422f479918b
Vadym Spitsyn <vspitsyn@solvd.com>
2025-07-17 14:34:49 +0200

    complited basic rest

--- internal/api/handler.go
+++ internal/api/handler.go
@@ -1,14 +1,37 @@
 package api
 
 import (
+	"encoding/json"
+
+	"net/http"
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"github.com/spitsynv2/yt-audio-cutter/internal/model"
-	"net/http"
-	"time"
 )
 
 var inMemoryJobs = make(map[string]model.Job) // temporary in-memory storage
+
+func GetStartPage(c *gin.Context) {
+	endpoints := []struct {
+		Name     string `json:"name"`
+		Endpoint string `json:"endpoint"`
+	}{
+		{"GET server status", "/health"},
+		{"GET jobs", "/jobs"},
+		{"GET job by id", "/jobs/id"},
+		{"POST create job", "/jobs"},
+	}
+
+	prettyJSON, err := json.MarshalIndent(endpoints, "", "  ")
+	if err != nil {
+		c.String(http.StatusInternalServerError, "JSON marshal error: %s", err)
+		return
+	}
+
+	c.Data(http.StatusOK, "application/json", prettyJSON)
+}
 
 func CreateJob(c *gin.Context) {
 	var jobInput model.Job
@@ -55,4 +78,55 @@ func GetJobs(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, jobs)
+}
+
+func UpdateJob(c *gin.Context) {
+	id := c.Param("id")
+
+	job, exists := inMemoryJobs[id]
+
+	if !exists {
+		c.JSON(http.StatusNotFound, gin.H{"error": "job not found"})
+		return
+	}
+
+	if job.Status != model.StatusPending {
+		c.JSON(http.StatusConflict, gin.H{"error": "cannot apdate not pending jobs"})
+		return
+	}
+
+	var updateInput struct {
+		StartTime *model.Duration `json:"start_time" binding:"required"`
+		EndTime   *model.Duration `json:"end_time" binding:"required"`
+	}
+
+	if err := c.ShouldBindJSON(&updateInput); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	job.StartTime = updateInput.StartTime
+	job.EndTime = updateInput.EndTime
+	inMemoryJobs[id] = job
+
+	c.JSON(http.StatusOK, job)
+}
+
+func DeleteJob(c *gin.Context) {
+	id := c.Param("id")
+
+	job, exists := inMemoryJobs[id]
+
+	if !exists {
+		c.JSON(http.StatusNotFound, gin.H{"error": "job not found"})
+		return
+	}
+
+	if job.Status != model.StatusPending {
+		c.JSON(http.StatusConflict, gin.H{"error": "cannot delete job once processing has started"})
+		return
+	}
+
+	delete(inMemoryJobs, id)
+	c.Status(http.StatusNoContent)
 }
--- internal/api/router.go
+++ internal/api/router.go
@@ -1,16 +1,18 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func RegisterRoutes(router *gin.Engine) {
 	router.GET("/health", func(c *gin.Context) {
 		c.JSON(200, gin.H{"status": "ok"})
 	})
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{"server": "running"})
-	})
+	router.GET("/", GetStartPage)
 
 	router.POST("/jobs", CreateJob)
 	router.GET("/jobs/:id", GetJobById)
 	router.GET("/jobs", GetJobs)
+	router.PUT("/jobs/:id", UpdateJob)
+	router.DELETE("/jobs/:id", DeleteJob)
 }
